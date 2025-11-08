e38d0d44d80e3c00ec7a021da634ffd279921d0c
stavrosflou <122636897+stavrosflou@users.noreply.github.com>
2025-11-08 17:59:36 +0200

    remove unused files

--- gallery_backend/images/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import gallery_view, random_gallery_view
-
-urlpatterns = [
-    path('gallery/', gallery_view, name='gallery'),
-    path('gallery/<int:id>/', gallery_view, name='gallery-detail'),
-    path('gallery/random/', random_gallery_view, name='gallery-random'),
-]
